05e4a92ece7b9bec81db3fabec10378b560b37ec
Kaio Márcio Araújo Cavalcante Lira <kaiomacl.20@gmail.com>
2025-03-30 14:31:35 -0300

    Receita 01 - Exercício 11

--- lib/main.dart
+++ lib/main.dart
@@ -8,34 +8,64 @@ void main() {
 
       home: Scaffold(
 
-        appBar: AppBar(title: const Text("Cervejas")),
+        appBar: AppBar(title: Text("Cervejas")),
 
-        body: DataTable(
-          columns: const [
-            DataColumn(label: Text('Nome')),
-            DataColumn(label: Text('Estilo')),
-            DataColumn(label: Text('IBU'))
-          ],
-          rows: const [
-            DataRow(cells: [
-              DataCell(Text('La Fin Du Monde')),
-              DataCell(Text('Bock')),
-              DataCell(Text('65')),
-            ]),
-            DataRow(cells: [
-              DataCell(Text('Sapporo Premium')),
-              DataCell(Text('Sour Ale')),
-              DataCell(Text('54')),
-            ]),
-            DataRow(cells: [
-              DataCell(Text('Duvel')),
-              DataCell(Text('Pilsner')),
-              DataCell(Text('82')),
-            ]),
-          ],
+        body: Scrollbar(
+          thumbVisibility: true,
+          child: SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: DataTable(
+              columns: [
+                DataColumn(label: Text('Nome')),
+                DataColumn(label: Text('Estilo')),
+                DataColumn(label: Text('IBU')),
+                DataColumn(label: Text('IBU')),
+                DataColumn(label: Text('IBU')),
+                DataColumn(label: Text('IBU')),
+                DataColumn(label: Text('IBU')),
+                DataColumn(label: Text('IBU')),
+                DataColumn(label: Text('IBU')),
+              ],
+              rows: [
+                DataRow(cells: [
+                  DataCell(Text('La Fin Du Monde')),
+                  DataCell(Text('Bock')),
+                  DataCell(Text('65')),
+                  DataCell(Text('65')),
+                  DataCell(Text('65')),
+                  DataCell(Text('65')),
+                  DataCell(Text('65')),
+                  DataCell(Text('65')),
+                  DataCell(Text('65')),
+                ]),
+                DataRow(cells: [
+                  DataCell(Text('Sapporo Premium')),
+                  DataCell(Text('Sour Ale')),
+                  DataCell(Text('54')),
+                  DataCell(Text('54')),
+                  DataCell(Text('54')),
+                  DataCell(Text('54')),
+                  DataCell(Text('54')),
+                  DataCell(Text('54')),
+                  DataCell(Text('54')),
+                ]),
+                DataRow(cells: [
+                  DataCell(Text('Duvel')),
+                  DataCell(Text('Pilsner')),
+                  DataCell(Text('82')),
+                  DataCell(Text('82')),
+                  DataCell(Text('82')),
+                  DataCell(Text('82')),
+                  DataCell(Text('82')),
+                  DataCell(Text('82')),
+                  DataCell(Text('82')),
+                ]),
+              ],
+            ),
+          ),
         ),
 
-        bottomNavigationBar: const Text("Botão 1"),
+        bottomNavigationBar: Text("Botão 1"),
 
       ));
 
