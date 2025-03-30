8b9020ca2d9a9f8e20f3893cd07c2d270e9bef08
Kaio Márcio Araújo Cavalcante Lira <kaiomacl.20@gmail.com>
2025-03-30 09:55:48 -0300

    Receita 01 - Exercício 10

--- lib/main.dart
+++ lib/main.dart
@@ -1,67 +1,44 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  MaterialApp app = MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.amber,
-        fontFamily: "Tektur"
-      ),
-      home: Scaffold(
-        appBar: AppBar(title: Text("Meu App")),
-        body: Center(
-          child: Column(
-            children: [
-              Text(
-                "Apenas começando",
-                style: TextStyle(fontWeight: FontWeight.bold)
-                ),
-              Text(
-                "No meio",
-                style: TextStyle(fontStyle: FontStyle.italic)
-                ),
-              FadeInImage.assetNetwork(
-                placeholder: '../assets/images/gradient-5812_256.gif',
-                image:'https://i.postimg.cc/4dDF24P4/imagem-2025-03-26-135052773.png',
-                fadeInDuration: const Duration(seconds: 1),
-                width: 300,
-                height: 200,
 
-              )
-            ]
-          )
+  MaterialApp app = MaterialApp(
+
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+
+      home: Scaffold(
+
+        appBar: AppBar(title: const Text("Cervejas")),
+
+        body: DataTable(
+          columns: const [
+            DataColumn(label: Text('Nome')),
+            DataColumn(label: Text('Estilo')),
+            DataColumn(label: Text('IBU'))
+          ],
+          rows: const [
+            DataRow(cells: [
+              DataCell(Text('La Fin Du Monde')),
+              DataCell(Text('Bock')),
+              DataCell(Text('65')),
+            ]),
+            DataRow(cells: [
+              DataCell(Text('Sapporo Premium')),
+              DataCell(Text('Sour Ale')),
+              DataCell(Text('54')),
+            ]),
+            DataRow(cells: [
+              DataCell(Text('Duvel')),
+              DataCell(Text('Pilsner')),
+              DataCell(Text('82')),
+            ]),
+          ],
         ),
-        bottomNavigationBar: Row(
-            children: [
-              Expanded(
-                child: IconButton(
-                  iconSize: 30,
-                  icon: Icon(Icons.house),
-                  onPressed: () {},
-                )
-              ),
-              Expanded(
-                child: ElevatedButton(
-                child: const Text("Botão 2"),
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.amber,
-                  foregroundColor: Colors.black,
-                )
-              )
-              ),
-              Expanded(
-                child: ElevatedButton(
-                child: const Text("Botão 3"),
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.amber,
-                  foregroundColor: Colors.black,
-                )
-              )
-              ),
-              
-            ]
-          )
+
+        bottomNavigationBar: const Text("Botão 1"),
+
       ));
+
   runApp(app);
+
 }
