6a92742990f0ed146df6eb709327ac39d8ea04f3
Kaio Márcio Araújo Cavalcante Lira <kaiomacl.20@gmail.com>
2025-05-12 09:07:35 -0300

    Receita 05

--- lib/main.dart
+++ lib/main.dart
@@ -1,230 +1,80 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
-var dataCoffee = [
-  {
-    "name": "Santa Clara",
-    "type": "Gourmet",
-    "price": "15,99"
-  },
-  {
-    "name": "Santa Helena",
-    "type": "Premium",
-    "price": "13,99"
-  },
-  {
-    "name": "3 Corações",
-    "type": "Extraforte",
-    "price": "11,99"
-  },
-];
-
-var dataObjects = [
-    {
-      "name": "La Fin Du Monde",
-      "style": "Bock",
-      "ibu": "65"
-    },
-    {
-      "name": "Sapporo Premiume",
-      "style": "Sour Ale",
-      "ibu": "54"
-    },
-    {
-      "name": "Duvel", 
-      "style": "Pilsner", 
-      "ibu": "82"
-    },
-    {
-      "name": "La Fin Du Monde",
-      "style": "Bock",
-      "ibu": "65"
-    },
-    {
-      "name": "Sapporo Premiume",
-      "style": "Sour Ale",
-      "ibu": "54"
-    },
-    {
-      "name": "Duvel", 
-      "style": "Pilsner", 
-      "ibu": "82"
-    },
-    {
-      "name": "La Fin Du Monde",
-      "style": "Bock",
-      "ibu": "65"
-    },
-    {
-      "name": "Sapporo Premiume",
-      "style": "Sour Ale",
-      "ibu": "54"
-    },
-    {
-      "name": "Duvel", 
-      "style": "Pilsner", 
-      "ibu": "82"
-    },
-    {
-      "name": "La Fin Du Monde",
-      "style": "Bock",
-      "ibu": "65"
-    },
-    {
-      "name": "Sapporo Premiume",
-      "style": "Sour Ale",
-      "ibu": "54"
-    },
-    {
-      "name": "Duvel", 
-      "style": "Pilsner", 
-      "ibu": "82"
-    },
-    {
-      "name": "La Fin Du Monde",
-      "style": "Bock",
-      "ibu": "65"
-    },
-    {
-      "name": "Sapporo Premiume",
-      "style": "Sour Ale",
-      "ibu": "54"
-    },
-    {
-      "name": "Duvel", 
-      "style": "Pilsner", 
-      "ibu": "82"
-    },
-    {
-      "name": "La Fin Du Monde",
-      "style": "Bock",
-      "ibu": "65"
-    },
-    {
-      "name": "Sapporo Premiume",
-      "style": "Sour Ale",
-      "ibu": "54"
-    },
-    {
-      "name": "Duvel", 
-      "style": "Pilsner", 
-      "ibu": "82"
-    }
-  ];
+var dataObjects = [];
 
 void main() {
-
   MyApp app = MyApp();
-
   runApp(app);
-
 }
 
 class MyApp extends StatelessWidget {
 
   @override
-
   Widget build(BuildContext context) {
-
-    
-
+    print("no build da classe MyApp"); 
     return MaterialApp(
-
       theme: ThemeData(primarySwatch: Colors.deepPurple),
-
       debugShowCheckedModeBanner:false,
-
       home: Scaffold(
-
         appBar: AppBar( 
-
           title: const Text("Dicas"),
-
           ),
-
-        body: MytileWidget(
-          items: dataCoffee,
-          fields: ["name", "type", "price"],
-        ),
-
+        body: DataTableWidget(jsonObjects:dataObjects),
         bottomNavigationBar: NewNavBar(),
-
       ));
-
   }
-
 }
 
-
-
-
-
-class NewNavBar extends StatelessWidget {
-
+class NewNavBar extends HookWidget {
   NewNavBar();
 
-
-
-  void botaoFoiTocado(int index) {
-
-    print("Tocaram no botão $index");
-
-  }
-
-
-
   @override
-
   Widget build(BuildContext context) {
-
-    return BottomNavigationBar(onTap: botaoFoiTocado, items: const [
-
-      BottomNavigationBarItem(
-
-        label: "Cafés",
-
-        icon: Icon(Icons.coffee_outlined),
-
-      ),
-
-      BottomNavigationBarItem(
-
-          label: "Cervejas", icon: Icon(Icons.local_drink_outlined)),
-
-      BottomNavigationBarItem(label: "Nações", icon: Icon(Icons.flag_outlined))
-
-    ]);
-
+    print("no build da classe NewNavBar");
+    var state = useState(1);
+    return BottomNavigationBar(
+      onTap: (index) {
+        state.value = index;
+      },
+      currentIndex: state.value,    
+      items: const [
+        BottomNavigationBarItem(
+          label: "Cafés",
+          icon: Icon(Icons.coffee_outlined),
+        ),
+        BottomNavigationBarItem(
+            label: "Cervejas", icon: Icon(Icons.local_drink_outlined)),
+        BottomNavigationBarItem(label: "Nações", icon: Icon(Icons.flag_outlined))
+      ]);
   }
-
 }
 
-
-
-class MytileWidget extends StatelessWidget {
-  final List<Map<String, dynamic>> items;
-  final List<String> fields;
-
-  const MytileWidget({
-    super.key,
-    required this.items,
-    required this.fields,
-  });
+class DataTableWidget extends StatelessWidget {
+  final List jsonObjects;
+  DataTableWidget( {this.jsonObjects = const [] });
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: items.length,
-      itemBuilder: (context, index) {
-        var item = items[index];
+    print("no build da classe DataTableWidget");
+    var columnNames = ["Nome","Estilo","IBU"],
+        propertyNames = ["name", "style", "ibu"];
 
-        return ListTile(
-          leading: const Icon(Icons.list),
-          title: Text(item[fields[0]]?.toString() ?? ''),
-          subtitle: Text(fields.skip(1).map((f) =>
-              '${f[0].toUpperCase()}${f.substring(1)}: ${item[f] ?? ""}').join(', ')),
-          trailing: const Icon(Icons.arrow_forward_ios),
-        );
-      },
-    );
+    return DataTable(
+      columns: columnNames.map( 
+                (name) => DataColumn(
+                  label: Expanded(
+                    child: Text(name, style: TextStyle(fontStyle: FontStyle.italic))
+                  )
+                )
+              ).toList()       
+      ,
+      rows: jsonObjects.map( 
+        (obj) => DataRow(
+            cells: propertyNames.map(
+              (propName) => DataCell(Text(obj[propName]))
+            ).toList()
+          )
+        ).toList());
   }
 }
